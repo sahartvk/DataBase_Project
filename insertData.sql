f8f18405373ce700d57f4913b7a918b8ae42227c
Fatemeh Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2023-01-16 21:44:50 +0330

    edit data

--- insertData.sql
+++ insertData.sql
@@ -1,12 +1,12 @@
 --insert customer
 insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (1, 'Lynn Gerretsen', '1273819144', '2002-01-09', '598 Holy Cross Avenue', '8435133118');
 insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (2, 'Truman Martinson', '1283106167', '2002-03-19', '3 Village Green Place', '7201423917');
-insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (3, 'Annecorinne Danihelka', '1245621450', '2001-12-23', '99909 Haas Plaza', '7968104125');
-insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (4, 'Geoffry Christaeas', '2541361453', '2010-12-25', '7652 Superior Drive', '6184473937');
+insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (3, 'Annecorinne Danihelka', '1245621459', '2001-12-23', '99909 Haas Plaza', '7968104125');
+insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (4, 'Geoffry Christaeas', '2541361459', '2010-12-25', '7652 Superior Drive', '6184473937');
 insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (5, 'Corri Latter', '1245864122', '2000-03-12', '32 Oriole Point', '6456453912');
-insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (6, 'Cleavland Winterscale', '1254625185', '1980-10-23', '632 Nelson Junction', '1584212360');
-insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (7, 'Georgie Bertie', '1326523241', '1960-01-14', '3658 Grover Parkway', '8704993907');
-insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (8, 'Erasmus Emett', '2145621521', '1980-01-02', '69 Vermont Junction', '3106105409');
+insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (6, 'Cleavland Winterscale', '1254625186', '1980-10-23', '632 Nelson Junction', '1584212360');
+insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (7, 'Georgie Bertie', '1326523244', '1960-01-14', '3658 Grover Parkway', '8704993907');
+insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (8, 'Erasmus Emett', '2145621520', '1980-01-02', '69 Vermont Junction', '3106105409');
 insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (9, 'Harlie Streak', '1265412302', '2002-12-07', '8 Delladonna Hill', '9394063351');
 insert into customer (CID, name, NatCod, Birthdate, Add_, Tel) values (10, 'Ginger Hanna', '1671421521', '1987-10-28', '6309 Linden Trail', '1758317779');
 
