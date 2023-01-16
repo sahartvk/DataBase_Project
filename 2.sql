96f47ad32c7361f27a4303fb92de81f96a65ee2f
Fatemeh-Soleimani <fsoleimani79@yahoo.com>
2023-01-16 23:48:17 +0330

    without error

--- 2.sql
+++ 2.sql
@@ -5,21 +5,25 @@ create procedure getTransaction
 as 
 begin
 
-with RECURSIVE Recdest (SourceDep,DesDep) as (
+with Recdest as (
 select SourceDep,DesDep,TrnDate
 from Trn_Src_Dec where Trn_Src_Dec.SourceDep=@sourceDep and Trn_Src_Dec.DesDep=@destDep
 
-union 
-select Recdest.SourceDep,Recdest.DesDep,Recdest.TrnDate
-from Trn_Src_Dec,Recdest
-where Recdest.SourceDep = Trn_Src_Dec.DesDep  and Recdest.TrnDate>=Trn_Src_Dec.TrnDate
-order by Recdest.TrnDate,Recdest.Amount
+union all
+select e.SourceDep,e.DesDep,e.TrnDate
+from Trn_Src_Dec as e 
+inner join 
+Recdest as r 
+on r.SourceDep = e.DesDep
+where r.TrnDate>=e.TrnDate
 
-union 
-select Recdest.SourceDep,Recdest.DesDep,Recdest.TrnDate
-from Trn_Src_Dec,Recdest
-where Recdest.DesDep = Trn_Src_Dec.SourceDep and Recdest.TrnDate<=Trn_Src_Dec.TrnDate
---previous transaction
+union all
+select t.SourceDep,t.DesDep,t.TrnDate
+from Trn_Src_Dec as t
+inner join 
+Recdest as r2 
+on r2.DesDep = t.SourceDep
+where r2.TrnDate<=t.TrnDate--previous transaction
 )
 
 select * from Recdest
@@ -28,3 +32,7 @@ select * from Recdest
 end
 
 EXEC getTransaction @SourceDep=25, @destDep=87
+
+
+-------------------------------------------------------------------------------------------------------------------
+
